122e4b3bd461703868f16b38bb5041b86c077db8
Chatchawan Sama <chatchawan.s@arise.tech>
2024-07-11 23:32:43 +0700

    Can getTodo, getTodoByID, putTodoByID

---
+++ database/database.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func ConnectDB() {
+	url := os.Getenv("DATABASE_URL")
+	var err error
+	DB, err = sql.Open("postgres", url)
+	if err != nil {
+		log.Fatal("Connect to database error", err)
+	}
+	if err = DB.Ping(); err != nil {
+		log.Fatal("Ping database error:", err)
+	}
+	fmt.Println("Connected to the database successfully!")
+}
+
+func CreateTable() {
+	createTb := `
+    CREATE TABLE IF NOT EXISTS todos (
+        id SERIAL PRIMARY KEY,
+        title TEXT,
+        done BOOLEAN
+    );
+    `
+	_, err := DB.Exec(createTb)
+	if err != nil {
+		log.Fatal("Can't create table", err)
+	}
+	fmt.Println("Create table success")
+}
--- server.go
+++ server.go
@@ -8,8 +8,11 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 	"time"
+
+	"todo-pnong/database"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -22,10 +25,53 @@ type Todo struct {
 	Done  bool   `json:"done"`
 }
 
+func getTodo(ctx *gin.Context) {
+	fmt.Println("Entering getTodo handler")
+	todos := []Todo{}
+
+	rows, err := database.DB.Query("SELECT id, title, done FROM todos")
+	if err != nil {
+		log.Fatal("can't query all todos", err)
+	}
+
+	for rows.Next() {
+		var id int
+		var title string
+		var done bool
+		err := rows.Scan(&id, &title, &done)
+		if err != nil {
+			log.Fatal("can't Scan row into variable", err)
+		}
+		fmt.Println(id, title, done)
+		todos = append(todos, Todo{id, title, done})
+	}
+
+	fmt.Println("query all todos success")
+	ctx.JSON(http.StatusOK, todos)
+
+}
+
+func getTodoByID(ctx *gin.Context) {
+	rowId := ctx.Param("id")
+
+	q := "SELECT id, title, done FROM todos where id=$1"
+	row := database.DB.QueryRow(q, rowId)
+	var id int
+	var title string
+	var done bool
+
+	err := row.Scan(&id, &title, &done)
+	if err != nil {
+		log.Fatal("can't Scan row into variables", err)
+	}
+
+	fmt.Println("one row", id, title, done)
+	ctx.JSON(http.StatusOK, Todo{id, title, done})
+}
+
 func postTodo(ctx *gin.Context) {
 	fmt.Println("Entering postTodo handler")
 	var todo Todo
-	todos := []Todo{}
 
 	if err := ctx.BindJSON(&todo); err != nil {
 		fmt.Println("Error binding JSON:", err)
@@ -33,9 +79,49 @@ func postTodo(ctx *gin.Context) {
 		return
 	}
 
-	todos = append(todos, todo)
-	fmt.Println("Todos list updated:", todos)
+	query := "INSERT INTO todos (title, done) VALUES ($1, $2) RETURNING id"
+	err := database.DB.QueryRow(query, todo.Title, todo.Done).Scan(&todo.ID)
+	if err != nil {
+		fmt.Println("Error inserting new todo:", err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	fmt.Println("Todo created with ID:", todo.ID)
 	ctx.JSON(http.StatusCreated, todo)
+}
+
+func putTodoByID(ctx *gin.Context) {
+	fmt.Println("Entering putTodoByID handler")
+
+	rowId := ctx.Param("id")
+
+	// var title string
+	// var done bool
+	var todo Todo
+
+	if err := ctx.BindJSON(&todo); err != nil {
+		fmt.Println("Error binding JSON:", err)
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	query := "UPDATE todos SET title=$2, done=$3 WHERE id=$1 RETURNING id;"
+	if _, err := database.DB.Exec(query, rowId, todo.Title, todo.Done); err != nil {
+		fmt.Println("Error executing update:", err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	idInt, err := strconv.Atoi(rowId)
+
+	if err != nil {
+		fmt.Println("Error during conversion")
+		return
+	}
+
+	fmt.Println("update success")
+	ctx.JSON(http.StatusCreated, Todo{idInt, todo.Title, todo.Done})
 }
 
 func main() {
@@ -44,13 +130,23 @@ func main() {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
+	// Connect to the database
+	database.ConnectDB()
+	defer database.DB.Close()
+
+	// Create tables
+	database.CreateTable()
+
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
 	r := gin.Default()
+	r.GET("/api/v1/todos", getTodo)
+	r.GET("/api/v1/todos/:id", getTodoByID)
 	r.POST("/api/v1/todos", postTodo)
+	r.PUT("/api/v1/todos/:id", putTodoByID)
 
-	port := os.Getenv("PORT")
+	port := os.Getenv("HOST")
 	// if port == "" {
 	// 	fmt.Println("Why Port Is String?!!")
 	// 	port = "8080" // default port if not specified
